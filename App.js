e7518c4f072665645bb4bf0e9735f1e9e40c899f
Kevin Tran <sesouk@gmail.com>
2020-04-09 21:26:37 -0600

    added input validation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, FlatList,  } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native'
 import Header from './components/Header'
 import ListItem from './components/ListItem'
 import AddItem from './components/AddItem'
@@ -20,9 +20,13 @@ const App = () => {
 
   }
   const addItem = (text) => {
-    setItems(prevItems => {
-      return [...prevItems, {id: Math.floor(Math.random() * 100000), text}]
-    })
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'Ok'})
+    } else {
+      setItems(prevItems => {
+        return [...prevItems, {id: Math.floor(Math.random() * 100000), text}]
+      })
+    }
   }
 
   return (
