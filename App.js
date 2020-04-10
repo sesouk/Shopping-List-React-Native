cdfd36bfeb524d30a858e0713d23bb2cbe48e1a8
Kevin Tran <sesouk@gmail.com>
2020-04-09 20:08:08 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>
+        Hello world!
+      </Text>
+    </View>
+  )
+}
+
+export default App
